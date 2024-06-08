7d3d050398fe81bd53449bb3d005d7d2780f696f
yuantamang <yuantamang@gmail.com>
2024-06-08 19:56:01 +0545

    some icons

--- app/Filament/Resources/DoumentResource.php
+++ app/Filament/Resources/DoumentResource.php
@@ -17,7 +17,7 @@ class DoumentResource extends Resource
 {
     protected static ?string $model = Doument::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document';
 
     public static function form(Form $form): Form
     {
