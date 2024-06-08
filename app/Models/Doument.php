fab3fab5e345d897b127ede97de89185fa8f1ce6
yuantamang <yuantamang@gmail.com>
2024-06-08 18:27:17 +0545

    database

---
+++ app/Models/Doument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doument extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_06_08_121520_create_post_photos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_photos', function (Blueprint $table) {
+            $table->id();
+            $table->string("names");
+            $table->string("title");
+            $table->string("image");
+            $table->foreignId("wedding_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_photos');
+    }
+};
